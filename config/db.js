605617434353dc5a22815751982f88c5bffb20c2
Alexander Micic <alexander.micic@gmail.com>
2018-02-15 20:03:18 +0100

    Update db

--- config/db.js
+++ config/db.js
@@ -1,6 +1,12 @@
+// module.exports = {
+//  secret: "jiraapplication",
+//  url: "mongodb://localhost/jira"
+// }
+
+// For https://library-angular2.herokuapp.com we are using remote database
 module.exports = {
-  secret: "jiraapplication",
-  url: "mongodb://localhost/jira"
+	secret: "jiraapplication",
+	url: "mongodb://alexm2018:node123@cluster0-shard-00-00-ezymz.mongodb.net:27017,cluster0-shard-00-01-ezymz.mongodb.net:27017,cluster0-shard-00-02-ezymz.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
 }
 
 // Note: if the local connection fails then
